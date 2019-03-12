41190f804a21593f84ddd837273121f2b7a53b7f
gauthier <gauthier.sayarath@gmail.com>
2019-03-12 18:19:12 +0100

    application index in progress

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,5 +1,28 @@
 class ApplicationsController < ApplicationController
   def index
+    @applications = Application.all
+
+    @identified = []
+    @applications.each do |application|
+      if application.status == "identified"
+        @identified << application
+      end
+    end
+
+    @applied = []
+    @applications.each do |application|
+      if application.status == "applied"
+        @applied << application
+      end
+    end
+
+    @in_progress = []
+    @applications.each do |application|
+      if application.status == "identified"
+        @in_progress << application
+      end
+    end
+
   end
 
   def show
