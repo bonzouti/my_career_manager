ad94657ce05274353c89b6312888a33be7ca3917
ailu88 <eloise.emptoz@gmail.com>
2019-03-13 15:23:22 +0100

    add view 'create new step'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :applications
+  resources :applications 
+  resources :steps
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root "applications#index"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+
 end
