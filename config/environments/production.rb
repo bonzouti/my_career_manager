09be6d3069ef474a1f251e260626e56ca97ff7e4
berenice bourgon <berenice.bourgon@gmail.com>
2019-03-13 09:42:16 +0100

    corrected typo on config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,5 +93,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #Devise 
-  onfig.action_mailer.default_url_options = { :host => 'mycareermanager.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'mycareermanager.herokuapp.com' }
 end
