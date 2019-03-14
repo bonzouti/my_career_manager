29703125a920d1ee949a29324d53b068663e64ba
lebonvieuxlion <debaugelion@gmail.com>
2019-03-14 13:30:51 +0100

    Creation of a contact on index_contacts using a modal from our template

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,4 +7,21 @@ class ContactsController < ApplicationController
 
 
   end
+
+  def create
+
+  	@application = Application.find(params[:application_id])
+
+  	@contact = Contact.new(application: @application, first_name: params[:first_name], last_name: params[:last_name], position: params[:position], email: params[:email], phone: params[:phone])
+
+  	if @contact.save
+
+  		redirect_to application_contacts_path(@application)
+
+  	end
+
+
+  end
+
+
 end
