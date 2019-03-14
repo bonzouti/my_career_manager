af0b7fa1e13e248c435d726ae3d0be0f2ff14c7c
berenice bourgon <berenice.bourgon@gmail.com>
2019-03-14 18:16:46 +0100

    added index page for archived applications with if condition, still have to add links though

--- app/controllers/archived_controller.rb
+++ app/controllers/archived_controller.rb
@@ -2,6 +2,17 @@ class ArchivedController < ApplicationController
 
   def index
     @applications = current_user.applications
+    @archived = []
+    @applications.each do |application|
+      if application.status == "archived"
+        @archived << application
+      else
+      end
+    end  
+  end
+
+  def new
   end
 
 end
+
