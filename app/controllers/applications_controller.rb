57a36c0f9f69c691acbb9dfe78aca242150ed49f
gauthier <gauthier.sayarath@gmail.com>
2019-03-14 14:20:49 +0100

    job offer edit without pop up done

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -19,16 +19,31 @@ class ApplicationsController < ApplicationController
     end
 
   end
+  
+  def edit_application_details
+    @application = Application.find(params[:id])
+  end
+
+  def update_application_details
+    @application = Application.find(params[:id])
+
+    if @application.update(joboffer_link: params[:joboffer_link], joboffer_description: params[:joboffer_description])
+      redirect_to application_path(@application)
+    else
+      render :edit
+    end
+  end
+
 
   def show
     @application = Application.find(params[:id])
-    
-
   end
+
 
   def new
     @application = Application.new
   end
+
 
   def create
     @application = Application.new(company_name: params[:company_name], position: params[:position], status: params[:status], joboffer_link: params[:joboffer_link], joboffer_description: params[:joboffer_description])
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   resources :applications do
     resources :contacts, only: [:index]
     resources :steps
+    
+    member do
+      get 'edit_application_details'
+      put 'update_application_details'
+    end
+
   end
   
   devise_for :users
