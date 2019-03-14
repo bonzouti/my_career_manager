e37e406dc323cf0b889809597d546669369eb839
ailu88 <eloise.emptoz@gmail.com>
2019-03-14 17:07:09 +0100

    fix view archived applications count

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def show
+        
+    @applications = current_user.applications
+    @archived = []
+    
+    @applications.each do |application|
+      if application.status == "archived"
+        @archived << application
+      end
+    end
+    
+  end
+
+
 end
