47d38c267d67219ca664e7b1a51aceac919753ac
lebonvieuxlion <debaugelion@gmail.com>
2019-03-15 14:01:37 +0100

    Creation of bootstrap alert at the top of the screen on every page using the applications_helper and the application.html.erb in the layout. The bootstrap alerts have been added in the file contacts_controller.rb to appear on the page index#contacts.

--- app/helpers/applications_helper.rb
+++ app/helpers/applications_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationsHelper
+
+  def flash_class(level)
+    case level
+      when 'notice' then "alert alert-primary alert-dismissible bg-primary text-white border-0 fade show"
+      when 'success' then "alert alert-success alert-dismissible bg-success text-white border-0 fade show"
+      when 'error' then "alert alert-danger alert-dismissible bg-danger text-white border-0 fade show"
+      when 'alert' then "alert alert-warning alert-dismissible fade show"
+    end
+  end
+
+
 end
