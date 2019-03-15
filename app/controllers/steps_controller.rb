df385d2b9b6a3a6f69aca1023e7bb433241b28f7
lebonvieuxlion <debaugelion@gmail.com>
2019-03-15 22:50:29 +0100

    Adding of a checkbox for each step  of one application in order to carry on the validation of a step. The validation destroy the step and update the application.notes using an Ajax request to hide the checkbox and add content to the notes text area  without reloading.

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,18 +1,52 @@
 class StepsController < ApplicationController
+
     def new
         @step = Step.new
         @application=Application.find(params[:application_id])
     end 
 
+
     def create
-        @step = Step.new(date: params[:date], category: params[:category], title: params[:title], description: params[:description])
-        @application = Application.find(params[:application_id])
-        @step.application = @application
-    if @step.save
-      @application.update_status
-      redirect_to application_path(@application)
-    else
-      render :new
+
+    @step = Step.new(date: params[:date], category: params[:category], title: params[:title],
+     description: params[:description])
+
+    @application = Application.find(params[:application_id])
+    @step.application = @application
+
+        if @step.save
+          @application.update_status
+          redirect_to application_path(@application)
+        else
+          render :new
+        end
+
     end
+
+    def update
+
+        @application = Application.find(params[:application_id])
+        @step = Step.find(params[:id])
+        @step.status = true
+
+        if @step.save 
+
+            @current_notes = @application.notes
+            @addition_to_current_notes = "#{@step.date} : #{@step.category}"  
+
+            @application.notes = @current_notes + "\n\n" + @addition_to_current_notes
+            @application.save
+            @step.destroy
+
+        respond_to do |format|
+            format.html {redirect_to application_path(@application)}
+            format.js
+        end
+
+
+        end
+ 
     end 
+
+
 end
