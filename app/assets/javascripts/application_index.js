6d48354999b4dd23cdb763cd2631dc1a2c85c478
ailu88 <eloise.emptoz@gmail.com>
2019-03-20 17:57:33 +0100

    dragndropsecondattempt

--- app/assets/javascripts/application_index.js
+++ app/assets/javascripts/application_index.js
@@ -1,24 +1,30 @@
 $(document).ready(function() {
+
+  console.log("bienvenue sur la page Index Applications.")
   
   $(".task-header").click(function() {
     $(this).next().toggle(0)
   })
   
   $(".button-header-2").click(function() {
+    console.log("button-2 clicked");
     $("#task-list-two").toggle()
   })
 
   $(".button-header-3").click(function() {
-    $("#task-list-three").toggle()
+    console.log("button-3 clicked");
+    $("#task-list-three").toggle();
   })
 
   $(".button-header-4").click(function() {
+    console.log("button-4 clicked");
     $("#task-list-four").toggle()
   })
 
-  document.getElementById('sortable-test-1').addEventListener("turbolinks:load", function(){
-    
-    console.log("turbolinks detected")
+
+  
+    $("sortable-test-1").click(function(){
+    console.log("turbolinks detected");
     $("#sortable-test-1").sortable();
   
   });
