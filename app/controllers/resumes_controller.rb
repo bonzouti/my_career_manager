f26b58614c708bf10775d55a575299c31c5a1b58
lebonvieuxlion <debaugelion@gmail.com>
2019-03-21 22:07:57 +0100

    [BUG] On page show_user correction of a bug that displayed a success notification even when no file was uploaded

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -2,9 +2,21 @@ class ResumesController < ApplicationController
 
   def create
     @user = User.find(params[:user_id])
-    @user.resume.attach(params[:resume])
-    flash[:success] = "Great ! Your resume has been uploaded"
-    redirect_to(user_path(@user))
+
+    if params[:resume] != nil 
+
+    	@user.resume.attach(params[:resume])
+	    flash[:success] = "Great ! Your resume has been uploaded"
+	    redirect_to(user_path(@user))
+
+	else
+
+		flash[:error] = "Your resume couldn't been upload. Are you sure your file was uploaded ?"
+		redirect_to(user_path(@user))
+
+
+	end
+
   end
 
 end
