508a07be22f3c14c1e73db96535289d6b7c88c52
ailu88 <eloise.emptoz@gmail.com>
2019-03-21 17:15:36 +0100

    controller cleaning #2

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -5,52 +5,39 @@ class StepsController < ApplicationController
         @application=Application.find(params[:application_id])
     end 
 
-
     def create
-
-    @step = Step.new(date: params[:date], category: params[:category], title: params[:title],
-     description: params[:description])
-
-    @application = Application.find(params[:application_id])
-    @step.application = @application
-
-        if @step.save
-          @application.update_status
-          respond_to do |format|
-            format.html {redirect_to application_path(@application)}
-            format.js
-          end
-        else
-          render :new
-        end
-
+        @step = Step.new(date: params[:date], category: params[:category], title: params[:title],
+        description: params[:description])
+        @application = Application.find(params[:application_id])
+        @step.application = @application
+            if @step.save
+            @application.update_status
+            respond_to do |format|
+                format.html {redirect_to application_path(@application)}
+                format.js
+            end
+            else
+            render :new
+            end
     end
 
     def update
-    @step = Step.find(params[:id])
-    @application = Application.find(params[:application_id])
-
-    @step.update(title: params[:title], category: params[:category], description: params[:description], date: params[:date])
-    respond_to do |format|
-        format.html {redirect_to application_path(@application)}
-        format.js
+        @step = Step.find(params[:id])
+        @application = Application.find(params[:application_id])
+        @step.update(title: params[:title], category: params[:category], description: params[:description], date: params[:date])
+        respond_to do |format|
+            format.html {redirect_to application_path(@application)}
+            format.js
+        end
     end
-
-
-    end
-
 
     def validate
-
         @application = Application.find(params[:application_id])
         @step = Step.find(params[:id])
         @step.status = true
-
         if @step.save 
-
             @current_notes = @application.notes
             @addition_to_current_notes = "#{@step.date} : #{@step.category}"  
-
             if @current_notes == nil
                 @application.notes = @addition_to_current_notes
             else
@@ -61,25 +48,16 @@ class StepsController < ApplicationController
                 format.html {redirect_to application_path(@application)}
                 format.js
             end
-
-
         end
- 
     end 
 
-
     def destroy
-
         @application = Application.find(params[:application_id])
         @step = Step.find(params[:id])
-
         @step.destroy
         respond_to do |format|
             format.html {redirect_to application_path(@application)}
             format.js
         end
-
     end
-
-
 end
